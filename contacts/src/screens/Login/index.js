4b6c341f2804ac5eccaa27aaa5e111275fa22c1b
brimelee <ismail.ibrahimadeiza@gmail.com>
2022-07-05 17:52:41 +0100

    input field added

--- contacts/src/screens/Login/index.js
+++ contacts/src/screens/Login/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+import Container from '../../components/common/container/index';
+import Input from '../../components/common/input/index';
 
 const Login = () => {
+  const [value, onChangeText] = useState('Useless Placeholder');
   return (
-    <View>
-      <Text>Hi from Logins</Text>
-    </View>
+    <SafeAreaView>
+      <Container>
+        <Input
+          label="Username"
+          onChangeText={text => onChangeText(text)}
+          value={value}
+        />
+
+        <Input
+          label="Password"
+          onChangeText={text => onChangeText(text)}
+          value={value}
+          icon={<Text>HIDE</Text>}
+          iconPosition="right"
+        />
+      </Container>
+    </SafeAreaView>
   );
 };
 
